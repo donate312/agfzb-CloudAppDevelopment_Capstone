11997c04c5947594a9de2250d7119dbf590d08fb
david <david.onate312@gmail.com>
2023-11-19 16:22:51 -0500

    figure out bootstrap navigation!

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,23 +2,29 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
+from .views import (
+    index,
+    get_dealerships,
+    get_dealer_details,
+    add_review,
+    about,
+    contact,
+    login_request,
+    logout_request,
+    registration_request,
+)
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-
-    # path for about view
-
-    # path for contact us view
-
-    # path for registration
-
-    # path for login
-
-    # path for logout
-
+    path('', index, name='index'),
+    path('', get_dealerships, name='get_dealerships'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
+    path('login/', login_request, name='login_request'),
+    path('logout/', logout_request, name='logout_request'),
+    path('registration/', registration_request, name='registration_request'),
+    path('dealer/<int:dealer_id>/', get_dealer_details, name='get_dealer_details'),
+    path('dealer/<int:dealer_id>/add_review/', add_review, name='add_review'),
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,35 +7,85 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
+from django.shortcuts import render
 import logging
 import json
+from .models import reviews  # Import your Review model
+from .models import Dealer  # Import your Dealer model
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 
-# Create your views here.
+# Update the `get_dealerships` view
+def get_dealerships(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/index.html', context)
+
+# Update the `get_dealer_details` view
+def get_dealer_details(request, dealer_id):
+    dealer = get_object_or_404(Dealer, pk=dealer_id)
+    reviews = dealer.review_set.all()
+    context = {'dealer': dealer, 'reviews': reviews}
+    return render(request, 'djangoapp/dealer_details.html', context)
+
+# Update the `add_review` view
+def add_review(request, dealer_id):
+    dealer = get_object_or_404(Dealer, pk=dealer_id)
+
+    if request.method == 'POST':
+        rating = request.POST['rating']
+        comment = request.POST['comment']
+        Review.objects.create(dealer=dealer, rating=rating, comment=comment)
+        messages.success(request, 'Review added successfully.')
+        return redirect('djangoapp:get_dealer_details', dealer_id=dealer_id)
+
+    context = {'dealer': dealer}
+    return render(request, 'djangoapp/add_review.html', context)
+
+def index(request):
+    return render(request, 'djangoapp/index.html')
+
+def about(request):
+    context = {}
+    return render(request, 'djangoapp/about.html', context)
 
 
-# Create an `about` view to render a static about page
-# def about(request):
-# ...
+def contact(request):
+    context = {}
+    return render(request, 'djangoapp/contact.html', context)
 
 
-# Create a `contact` view to return a static contact page
-#def contact(request):
+def login_request(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful.')
+            return redirect('djangoapp:get_dealerships')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'djangoapp/login.html')
 
-# Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
 
-# Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    messages.success(request, 'Logout successful.')
+    return redirect('djangoapp:get_dealerships')
 
-# Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+
+def registration_request(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        User.objects.create_user(username=username, password=password)
+        messages.success(request, 'Registration successful. Please log in.')
+        return redirect('djangoapp:login_request')
+    return render(request, 'djangoapp/registration.html')
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
@@ -44,11 +94,28 @@ def get_dealerships(request):
         return render(request, 'djangoapp/index.html', context)
 
 
-# Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
 
-# Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
+
+def get_dealer_details(request, dealer_id):
+    dealer = get_object_or_404(Dealer, pk=dealer_id)
+    # Assuming you have a related model for reviews, adjust accordingly
+    reviews = dealer.review_set.all()  
+    context = {'dealer': dealer, 'reviews': reviews}
+    return render(request, 'djangoapp/dealer_details.html', context)
+
+
+
+
+def add_review(request, dealer_id):
+    dealer = get_object_or_404(Dealer, pk=dealer_id)
+    
+    if request.method == 'POST':
+        rating = request.POST['rating']
+        comment = request.POST['comment']
+        Review.objects.create(dealer=dealer, rating=rating, comment=comment)
+        messages.success(request, 'Review added successfully.')
+        return redirect('djangoapp:get_dealer_details', dealer_id=dealer_id)
+
+    context = {'dealer': dealer}
+    return render(request, 'djangoapp/add_review.html', context)
 
